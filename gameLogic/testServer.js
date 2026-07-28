660717fc91abc3ee1be8ca2b89ccd154da953acc
Alexander <kaoyung61@gmail.com>
2026-07-28 12:01:48 +0200

    2026-07-28-12-01_Fehlern changed

---
+++ gameLogic/testServer.js
@@ -0,0 +1,29 @@
+import { sendToPlayer } from "../network.js";
+
+const players = new Map();
+
+export function setName(socket, request) {
+
+    if (socket.playerName) {
+        players.delete(socket.playerName);
+    }
+    socket.playerName = request.name;
+    players.set(request.name, socket);
+
+    console.log(`${request.name} registered`);
+
+}
+
+export function message(socket, request) {
+    const target = players.get(request.to);
+    if (!target) {
+        console.log(`Player '${request.to}' not found`);
+        return;
+    }
+    target.send(JSON.stringify({
+        type: "message",
+        from: socket.playerName,
+        text: request.text
+    }));
+
+}
