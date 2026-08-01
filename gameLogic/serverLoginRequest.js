9efc2f66b040bfa3d9438e4958a860f65d7fbaf2
Alexander <kaoyung61@gmail.com>
2026-08-01 13:51:06 +0200

    e2  wsdfg

--- gameLogic/serverLoginRequest.js
+++ gameLogic/serverLoginRequest.js
@@ -1,15 +1,15 @@
 
 export function CR_authoriseMe(socket, request) {
-    console.log("CR_authoriseMe called with socket:", socket, " and request:", request);
+    console.log("CR_authoriseMe called with request:", request);
 }
 
 
 export function CR_newClientRegistration(socket, request) {
-    console.log("CR_newClientRegistration called with socket:", socket, " and request:", request);
+    console.log("CR_newClientRegistration called with request:", request);
 }
 
 
 export function CR_loginClient(socket, request) {
-    console.log("CR_loginClient called with socket:", socket, " and request:", request);
+    console.log("CR_loginClient called with request:", request);
 
 }
