55373cd2b571b7d259292b501a32a8b267334c8e
Alexander <kaoyung61@gmail.com>
2026-08-01 17:40:07 +0200

    sdfghgfd1234

--- gameLogic/serverLoginRequest.js
+++ gameLogic/serverLoginRequest.js
@@ -2,7 +2,7 @@ import { sendToSocket, sendToPlayer } from "../serverNetwork.js";
 import { readDatabaseObject } from "../serverDatabase.js";
 
 
-export function CR_loginClient(socket, request) {
+export async function CR_loginClient(socket, request) {
     console.log("CR_loginClient start");
     let player = await readDatabaseObject("players", "login", request.data.login, "*");
     console.log("CR_loginClient: player:", player);
@@ -15,14 +15,14 @@ export function CR_loginClient(socket, request) {
 
 }
 
-export function CR_authoriseMe(socket, request) {
+export async function CR_authoriseMe(socket, request) {
     console.log("CR_authoriseMe start");
     sendToSocket(socket, { command: "message", text: "CR_authoriseMe: Authorisation angefragt" });
     console.log("CR_authoriseMe end");
 }
 
 
-export function CR_newClientRegistration(socket, request) {
+export async function CR_newClientRegistration(socket, request) {
     console.log("CR_newClientRegistration start");
     sendToSocket(socket, { command: "message", text: "CR_newClientRegistration: Neuer Client registriert" });
     console.log("CR_newClientRegistration end");
