901180540ee35ac05901b87475bd07ab14696efd
Alexander <kaoyung61@gmail.com>
2026-08-01 16:43:26 +0200

    cxbcbcb

--- gameLogic/serverLoginRequest.js
+++ gameLogic/serverLoginRequest.js
@@ -4,7 +4,7 @@ import { sendToSocket, sendToPlayer } from "../serverNetwork.js";
 
 export function CR_loginClient(socket, request) {
     console.log("CR_loginClient start");
-    let player = await readDatabaseObject("players", "login", request.data.login, "*");
+    let player = readDatabaseObject("players", "login", request.data.login, "*");
     if (player.pasword === request.data.password) {
         sendToSocket(socket, { command: "token", token: player.token });
     } else {
